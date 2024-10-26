46589d1410c13e215c2867bc960ae649293ddaf5
Cpun077 <cadenpun@gmail.com>
2024-10-25 17:29:20 -0700

    slider basics

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
-import pygame
-import sys
+import sys, pygame, pygame_widgets
+from lib.parameter import parameter
 
 pygame.init()
 
-window_width, window_height = 800, 600
+window_width, window_height = 1200, 800
 window = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Pygame Window")
+pygame.display.set_caption("Habitable Exoplanet Simulation")
+
+width = window_width/5
+height = window_height/25
+dist = parameter(window, window_width/10, window_height/10, width, height, 10)
+params={dist}
+
+circle_color = (0, 128, 255)
+circle_radius = 200
+circle_position = (window_width/2, window_height/2)
 
 running = True
 while running:
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
         if event.type == pygame.QUIT:
             running = False
 
-    window.fill((255, 255, 255))
+    window.fill((0, 0, 0))
 
+    pygame.draw.circle(window, circle_color, circle_position, circle_radius)
+
+    pygame_widgets.update(events)
+    for param in params:
+        param.update()
     pygame.display.flip()
 
 pygame.quit()
